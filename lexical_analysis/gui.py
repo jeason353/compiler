4ed980b69631d97684b72882b8e8f8669b2847c0
wk <1398869507@qq.com>
2019-04-23 20:55:30 +0800

    lexical analysis

---
+++ lexical_analysis/gui.py
@@ -0,0 +1,53 @@
+from lexical_analysis import lexical
+import tkinter
+
+def on_click():
+    text.delete(1.0, 'end')  # 清空上次扫描结果
+    text_1.delete(1.0, 'end')
+    output = ''
+    output_1 = ''
+    path = filepath.get()
+
+    output, symbols = lexical(path)
+    for key, value in symbols.items():
+        output_1 += key + '\t' + str(value) + '\n'
+
+    text.insert(1.0, output)
+    text_1.insert(1.0, output_1)
+
+if __name__=='__main__':
+    root = tkinter.Tk()
+    root.title('词法分析')
+
+    fram = tkinter.Frame(root)
+    tkinter.Label(fram, text='输入文件', font=("Arial", 12), width=7, height=2).pack(side=tkinter.TOP)
+    filepath = tkinter.StringVar()
+    file = tkinter.Entry(fram, textvariable=filepath, bd=3)
+    filepath.set('')
+    file.pack(side=tkinter.TOP)
+    tkinter.Button(fram, text='词法分析', command=on_click, font=("Arial", 12), width=7, height=1).pack(side=tkinter.TOP)
+    fram.pack(side=tkinter.LEFT)
+
+    fram1 = tkinter.Frame(root)
+    tkinter.Label(fram1, text='token序列', font=("Arial", 12), width=7, height=2).pack(side=tkinter.TOP)
+    scroll = tkinter.Scrollbar(fram1)
+    scroll.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    text = tkinter.Text(fram1, font=("Arial", 10), width=30, height=20)
+    scroll.config(command=text.yview)
+    text.config(yscrollcommand=scroll.set)
+    text.pack(side=tkinter.LEFT)
+    fram1.pack(side='left')
+
+    fram2 = tkinter.Frame(root)
+    tkinter.Label(fram2, text='符号表', font=("Arial", 12), width=7, height=2).pack(side=tkinter.TOP)
+    scroll_1 = tkinter.Scrollbar(fram2)
+    scroll_1.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    text_1 = tkinter.Text(fram2, font=("Arial", 10), width=20, height=20)
+    scroll_1.config(command=text_1.yview)
+    text_1.config(yscrollcommand=scroll_1.set)
+    text_1.pack(side=tkinter.LEFT)
+    fram2.pack(side='left')
+
+
+    root.mainloop()
+
---
+++ lexical_analysis/test.cpp
@@ -0,0 +1,15 @@
+int main(){
+    bool b = true;
+    int x = 3;
+    float y = 2.5;
+    /*this is our annotations*/
+
+    if(x > 3){
+    	y++;
+    }
+
+    do{
+    	x++;
+    }while(x < 30);
+    return 0;
+}
